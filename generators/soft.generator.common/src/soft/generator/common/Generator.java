b57b674739c51d7238b8fde65c0c14f45497bdff
Jocelyn Dumay <jdy@masagroup.net>
2019-09-30 17:55:25 +0200

    [gen] convert fullpath jar URI to classpath URI in AcceleoUriHandler and convert classpath URI to fullpath jar URI in acceleo generator

--- generators/soft.generator.common/src/soft/generator/common/Generator.java
+++ generators/soft.generator.common/src/soft/generator/common/Generator.java
@@ -2,6 +2,7 @@ package soft.generator.common;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -20,14 +22,24 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.eclipse.acceleo.common.IAcceleoConstants;
+import org.eclipse.acceleo.common.internal.utils.AcceleoPackageRegistry;
+import org.eclipse.acceleo.common.utils.ModelUtils;
 import org.eclipse.acceleo.engine.event.AcceleoTextGenerationEvent;
 import org.eclipse.acceleo.engine.event.IAcceleoTextGenerationListener;
 import org.eclipse.acceleo.engine.service.AbstractAcceleoGenerator;
 import org.eclipse.acceleo.engine.service.AcceleoService;
+import org.eclipse.acceleo.model.mtl.Module;
+import org.eclipse.acceleo.model.mtl.resource.AcceleoResourceFactoryRegistry;
+import org.eclipse.acceleo.model.mtl.resource.AcceleoResourceSetImpl;
 import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.Resource.Factory;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -36,6 +48,20 @@ import com.google.common.collect.Maps;
  * Entry point of the 'Generate' generation module.
  */
 public class Generator extends AbstractAcceleoGenerator {
+
+    private class ResourceFactoryRegistry extends AcceleoResourceFactoryRegistry {
+        public ResourceFactoryRegistry(Resource.Factory.Registry delegate) {
+            super(delegate);
+        }
+
+        protected Factory delegatedGetFactory(URI uri, String contentTypeIdentifier) {
+            if ("classpath".equals(uri.scheme())) {
+                URL url = getClass().getResource(uri.path());
+                uri = createTemplateURI(url.toString());
+            }
+            return super.delegatedGetFactory(uri, contentTypeIdentifier);
+        }
+    }
 
     /**
      * The list of properties files from the launch parameters (Launch
@@ -68,17 +94,8 @@ public class Generator extends AbstractAcceleoGenerator {
      * @generated
      */
     private Generator(URI modelURI, File targetFolder, List<? extends Object> arguments) throws IOException {
-        initialize(modelURI, targetFolder, arguments);
-    }
-
-    public void initialize(URI modelURI, File targetFolder, List<? extends Object> arguments) throws IOException {
         initializeArguments(arguments);
-        super.initialize(modelURI, targetFolder, Collections.emptyList());
-    }
-
-    public void initialize(EObject model, File targetFolder, List<? extends Object> arguments) throws IOException {
-        initializeArguments(arguments);
-        super.initialize(model, targetFolder, Collections.emptyList());
+        initializeGenerator(modelURI, targetFolder, Collections.emptyList());
     }
 
     private void initializeArguments(List<? extends Object> arguments) {
@@ -90,6 +107,63 @@ public class Generator extends AbstractAcceleoGenerator {
         this.properties = properties;
         Boolean silent = (Boolean) arguments.get(3);
         this.silent = silent;
+    }
+
+    public void initializeGenerator(URI modelURI, File folder, List<? extends Object> arguments) throws IOException {
+        ResourceSet modulesResourceSet = new AcceleoResourceSetImpl();
+        modulesResourceSet.setPackageRegistry(AcceleoPackageRegistry.INSTANCE);
+
+        resourceFactoryRegistry = modulesResourceSet.getResourceFactoryRegistry();
+        modulesResourceSet.setResourceFactoryRegistry(new ResourceFactoryRegistry(resourceFactoryRegistry));
+
+        URIConverter uriConverter = createURIConverter();
+        if (uriConverter != null) {
+            modulesResourceSet.setURIConverter(uriConverter);
+        }
+
+        Map<URI, URI> uriMap = EcorePlugin.computePlatformURIMap(false);
+
+        // make sure that metamodel projects in the workspace override those in plugins
+        modulesResourceSet.getURIConverter().getURIMap().putAll(uriMap);
+
+        registerResourceFactories(modulesResourceSet);
+        registerPackages(modulesResourceSet);
+
+        ResourceSet modelResourceSet = new AcceleoResourceSetImpl();
+        modelResourceSet.setPackageRegistry(AcceleoPackageRegistry.INSTANCE);
+        if (uriConverter != null) {
+            modelResourceSet.setURIConverter(uriConverter);
+        }
+
+        // make sure that metamodel projects in the workspace override those in plugins
+        modelResourceSet.getURIConverter().getURIMap().putAll(uriMap);
+
+        registerResourceFactories(modelResourceSet);
+        registerPackages(modelResourceSet);
+
+        String moduleName = getModuleName();
+        if (moduleName.endsWith('.' + IAcceleoConstants.MTL_FILE_EXTENSION)) {
+            moduleName = moduleName.substring(0, moduleName.lastIndexOf('.'));
+        }
+        if (!moduleName.endsWith('.' + IAcceleoConstants.EMTL_FILE_EXTENSION)) {
+            moduleName += '.' + IAcceleoConstants.EMTL_FILE_EXTENSION;
+        }
+
+        URL moduleURL = findModuleURL(moduleName);
+
+        if (moduleURL == null) {
+            throw new IOException("'" + getModuleName() + ".emtl' not found"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        URI moduleURI = createTemplateURI(moduleURL.toString());
+        moduleURI = URI.createURI(moduleURI.toString(), true);
+        module = (Module) ModelUtils.load(moduleURI, modulesResourceSet);
+
+        URI newModelURI = URI.createURI(modelURI.toString(), true);
+        model = ModelUtils.load(newModelURI, modelResourceSet);
+        targetFolder = folder;
+        generationArguments = arguments;
+
+        this.postInitialize();
     }
 
     protected AcceleoService createAcceleoService() {
@@ -311,6 +385,28 @@ public class Generator extends AbstractAcceleoGenerator {
                        .put(org.eclipse.emf.ecore.EcorePackage.eINSTANCE.getNsURI(),
                             org.eclipse.emf.ecore.EcorePackage.eINSTANCE);
         }
+    }
+
+    protected URIConverter createURIConverter() {
+        return new ExtensibleURIConverterImpl() {
+            /**
+             * {@inheritDoc}
+             * 
+             * @see org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl#normalize(org.eclipse.emf.common.util.URI)
+             */
+            @Override
+            public URI normalize(URI uri) {
+                URI normalized = getURIMap().get(uri);
+                if (normalized == null) {
+                    if ("classpath".equals(uri.scheme())) {
+                        URL url = getClass().getResource(uri.path());
+                        normalized = createTemplateURI(url.toString());
+                        getURIMap().put(uri, normalized);
+                    }
+                }
+                return normalized != null ? normalized : super.normalize(uri);
+            }
+        };
     }
 
 }
