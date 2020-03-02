f44d91319f482d149bd2ca79cd4ad0e154f50adc
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2020-03-02 10:06:34 +0100

    [gen][cpp] ensure nio.FileSystem is created/destroyed correctly when creating Path in jar file

--- generators/soft.generator.cpp/src/soft/generator/cpp/Generate.java
+++ generators/soft.generator.cpp/src/soft/generator/cpp/Generate.java
@@ -1,12 +1,21 @@
 package soft.generator.cpp;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.ecore.EObject;
@@ -53,29 +62,73 @@ public class Generate extends Generator {
         }
     }
 
+    private static class PathReference implements AutoCloseable {
+
+        private Path p;
+        private FileSystem fs;
+
+        public PathReference(Path p, FileSystem fs) {
+            this.p = p;
+            this.fs = fs;
+        }
+
+        public void close() throws Exception {
+            if (this.fs != null)
+                this.fs.close();
+        }
+
+        public Path getPath() {
+            return p;
+        }
+    }
+
+    private static PathReference getPath(final URI resURI) throws IOException {
+        try {
+            // first try getting a path via existing file systems
+            return new PathReference(Paths.get(resURI), null);
+        } catch (final FileSystemNotFoundException e) {
+            /*
+             * not directly on file system, so then it's somewhere else (e.g.: JAR)
+             */
+            final Map<String, ?> env = Collections.emptyMap();
+            final FileSystem fs = FileSystems.newFileSystem(resURI, env);
+            return new PathReference(fs.provider().getPath(resURI), fs);
+        }
+    }
+
     protected void postGenerate(ResourceSet resourceSet) {
         if (Arrays.stream(getTemplateNames()).anyMatch(t -> t.equals("generateTestsCMakeProject"))) {
             EObject eModel = getModel();
             if (eModel instanceof EPackage) {
                 EPackage ePackage = (EPackage) eModel;
                 try {
-                    URL srcUrl = getClass().getResource("cmake");
-                    Path from = Paths.get(srcUrl.toURI()).normalize();
-                    Path to = Paths.get(targetFolder.toURI()).resolve(ePackage.getName() + "/tests/cmake").normalize();
-                    Files.walk(from).forEach(src -> {
-                        final Path dest = to.resolve(from.relativize(src).toString());
-                        try {
-                            if (Files.isDirectory(src)) {
-                                if (Files.notExists(dest)) {
-                                    System.out.println("creating directory:" + to);
-                                    Files.createDirectories(dest);
-                                }
-                            } else {
-                                System.out.println("extracting file:" + dest);
-                                Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                    URL srcURL = getClass().getResource("cmake");
+                    URI srcURI = srcURL.toURI();
+
+                    // for each file in cmake src resource directory, copy it to destination
+                    PathReference fromReference = getPath(srcURI);
+                    PathReference toReference = getPath(targetFolder.toURI());
+                    Path from = fromReference.getPath().normalize();
+                    Path to = toReference.getPath().resolve(ePackage.getName() + "/tests/cmake").normalize();
+
+                    Files.walkFileTree(from, new SimpleFileVisitor<Path>() {
+
+                        @Override
+                        public FileVisitResult preVisitDirectory(final Path dir, final BasicFileAttributes attrs)
+                                throws IOException {
+                            Path dest = to.resolve(from.relativize(dir).toString());
+                            System.out.println("creating directory:" + dest);
+                            Files.createDirectories(dest);
+                            return FileVisitResult.CONTINUE;
+                        }
+
+                        @Override
+                        public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs)
+                                throws IOException {
+                            Path dest = to.resolve(from.relativize(file).toString());
+                            System.out.println("extracting file:" + dest);
+                            Files.copy(file, dest, StandardCopyOption.REPLACE_EXISTING);
+                            return FileVisitResult.CONTINUE;
                         }
                     });
                 } catch (Exception e) {
