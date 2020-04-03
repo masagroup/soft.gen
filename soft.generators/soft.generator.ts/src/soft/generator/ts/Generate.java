18202b9408a99073625179caa7420c98dd62e1ad
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-04-03 10:12:22 +0200

    [gen][ts] create ts generator

---
+++ soft.generators/soft.generator.ts/src/soft/generator/ts/Generate.java
@@ -0,0 +1,40 @@
+package soft.generator.ts;
+
+import org.eclipse.emf.common.util.BasicMonitor;
+
+import soft.generator.common.Generator;
+
+/**
+ * Entry point of the 'Generate' generation module.
+ * 
+ * @generated NOT
+ */
+public class Generate extends Generator {
+    /**
+     * The name of the module.
+     */
+    public static final String MODULE_FILE_NAME = "/soft/generator/ts/generate";
+
+    /**
+     * The name of the templates that are to be generated.
+     */
+    public static final String[] TEMPLATE_NAMES = { "generateModel", "generateTests" };
+
+    private Generate() {
+        super(MODULE_FILE_NAME, TEMPLATE_NAMES);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Generate g = new Generate();
+            boolean p = g.parse(args);
+            if (p) {
+                g.initialize();
+                g.generate(new BasicMonitor());
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+}
